28d31d33beae0910048a9c509dbb06b417d65238
Reshma Ananda Prabhakar <rxa614@case.edu>
2025-03-19 21:30:28 -0400

    project-setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+export const signup = async (req,res) =>{
+    res.json({
+        data:"sign up hit",
+    });
+};
+
+
+export const login = async (req,res) =>{
+    res.json({
+        data:"sign up hit",
+    });
+};
+
+export const logout = async (req,res) =>{
+    res.json({
+        data:"sign up hit",
+    });
+};
